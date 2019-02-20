28b17124f7e19eedf5406db878169f4aa340429b
Deivid <musialik.dawid.programming@gmail.com>
2019-02-20 20:38:26 +0100

    Add product model. Add nuget for entity framework core and postgresql.

---
+++ SharedLib.Objects/ProductAbstract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib.Objects
+{
+    public class ProductAbstract
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
---
+++ SharedLib.Objects/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharedLib.Objects
+{
+    [Table("Product", Schema = "Task")]
+    public class ProductModel: ProductAbstract 
+    {
+        [Key]
+        public override Guid Id { get; set; }
+
+        public override string Name { get; set; }
+
+        public override decimal Price { get; set; }
+    }
+}
